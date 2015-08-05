910a7b6ceca5017ea623992cff519e5f41aa656d
Jevon Wright <jevon@powershop.co.nz>
2015-08-05 13:26:22 +1200

    Add Google OAuth2 login and sessions
    Based off http://www.jevon.org/wiki/Google_OAuth2_with_Ruby_on_Rails

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+# Use HAML for templating
+gem 'haml-rails'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
@@ -31,6 +33,12 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
+# OAuth2 login
+gem 'dotenv-rails'
+gem 'figaro'
+gem 'omniauth-google-oauth2'
+gem 'activerecord-session_store'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150805011948) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", limit: 255,   null: false
+    t.text     "data",       limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider",      limit: 255
+    t.string   "uid",           limit: 255
+    t.string   "name",          limit: 255
+    t.string   "refresh_token", limit: 255
+    t.string   "access_token",  limit: 255
+    t.datetime "expires"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
 end
