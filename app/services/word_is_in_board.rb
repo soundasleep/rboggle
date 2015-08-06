19652c955d98c320ad852a5645b81cad264ecabe
Jevon Wright <jevon@powershop.co.nz>
2015-08-06 13:50:54 +1200

    Implement word search

--- app/services/word_is_in_board.rb
+++ app/services/word_is_in_board.rb
@@ -7,7 +7,66 @@ class WordIsInBoard
   end
 
   def call
-    true
+    (0...board.height).each do |start_y|
+      (0...board.width).each do |start_x|
+        visited = create_empty_visited
+        remaining = word
+
+        x = start_x
+        y = start_y
+
+        # first characters
+        c = board.cell_at(x, y)
+        if remaining[0, c.length] == c
+          remaining = remaining.slice(c.length, remaining.length)
+          visited[y][x] = true
+
+          return true if remaining.empty?
+        else
+          next
+        end
+
+        # now search through for connecting characters
+        found_next = true
+        while !remaining.empty? && found_next
+          found_next = false
+
+          (-1..1).each do |dy|
+            (-1..1).each do |dx|
+              if x + dx >= 0 && y + dy >= 0 && x + dx < board.width && y + dy < board.height
+                if !visited[y + dy][x + dx]
+                  c = board.cell_at(x + dx, y + dy)
+                  if remaining[0, c.length] == c
+                    remaining = remaining.slice(c.length, remaining.length)
+                    visited[y + dy][x + dx] = true
+
+                    x = x + dx
+                    y = y + dy
+
+                    # we've found a match
+                    if remaining.empty?
+                      return true
+                    end
+
+                    found_next = true
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+
+    false
+  end
+
+  private
+
+  def create_empty_visited
+    board.cells.map do |row|
+      row.map { false }
+    end
   end
 
 end
---
+++ spec/services/word_is_in_board_spec.rb
@@ -0,0 +1,119 @@
+require "rails_helper"
+
+RSpec.describe WordIsInBoard, type: :service do
+  let(:game) { Game.create! }
+  let(:board) { game.boards.create! round_number: 1, finished: true, serialized: "c,a,t,b|d,o,g,z|z,z,z,z|qu,z,q,z" }
+  let(:result) { WordIsInBoard.new(board: board, word: word).call }
+
+  context "cat" do
+    let(:word) { "cat" }
+
+    it "returns true" do
+      expect(result).to be(true)
+    end
+  end
+
+  context "dog" do
+    let(:word) { "dog" }
+
+    it "returns true" do
+      expect(result).to be(true)
+    end
+  end
+
+  # diagonal
+  context "cot" do
+    let(:word) { "cot" }
+
+    it "returns true" do
+      expect(result).to be(true)
+    end
+  end
+
+  context "coat" do
+    let(:word) { "coat" }
+
+    it "returns true" do
+      expect(result).to be(true)
+    end
+  end
+
+  # going over the same square
+  context "dod" do
+    let(:word) { "dod" }
+
+    it "returns false" do
+      expect(result).to be(false)
+    end
+  end
+
+  context "o" do
+    let(:word) { "o" }
+
+    it "returns true" do
+      expect(result).to be(true)
+    end
+  end
+
+  # iterating on the same square
+  context "dood" do
+    let(:word) { "dood" }
+
+    it "returns false" do
+      expect(result).to be(false)
+    end
+  end
+
+  # jumping too far
+  context "dtz" do
+    let(:word) { "dtz" }
+
+    it "returns false" do
+      expect(result).to be(false)
+    end
+  end
+
+  # wrapping around
+  context "zcz" do
+    let(:word) { "zcz" }
+
+    it "returns false" do
+      expect(result).to be(false)
+    end
+  end
+
+  # characters that don't exist
+  context "cax" do
+    let(:word) { "cax" }
+
+    it "returns false" do
+      expect(result).to be(false)
+    end
+  end
+
+  # qu special case
+  context "quz" do
+    let(:word) { "quz" }
+
+    it "returns true" do
+      expect(result).to be(true)
+    end
+  end
+
+  context "zqu" do
+    let(:word) { "zqu" }
+
+    it "returns true" do
+      expect(result).to be(true)
+    end
+  end
+
+  context "qzq" do
+    let(:word) { "qzq" }
+
+    it "returns false" do
+      expect(result).to be(false)
+    end
+  end
+
+end
