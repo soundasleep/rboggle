9b20bb36aa9da9db12957590d80f310eaa545726
Jevon Wright <jevon@powershop.co.nz>
2015-08-07 16:25:31 +1200

    Rewrite WordIsInBoard to search for all trees

--- app/services/word_is_in_board.rb
+++ app/services/word_is_in_board.rb
@@ -7,73 +7,64 @@ class WordIsInBoard
   end
 
   def call
-    (0...board.height).each do |start_y|
-      (0...board.width).each do |start_x|
-        visited = create_empty_visited
-        remaining = word
-
-        x = start_x
-        y = start_y
-
-        # first characters
-        c = board.cell_at(x, y)
-
-        if remaining[0, c.length] == c
-          remaining = remaining.slice(c.length, remaining.length)
-          visited[y][x] = true
-
-          return true if remaining.empty?
-        else
-          # this x,y doesn't start the word
-          next
-        end
-
-        # now search through for connecting characters
-        found_next = true
-        while !remaining.empty? && found_next
-          found_next = false
-
-          (-1..1).each do |dy|
-            (-1..1).each do |dx|
-              if valid?(x + dx, y + dy)
-                if !visited[y + dy][x + dx]
-                  c = board.cell_at(x + dx, y + dy)
-
-                  if remaining[0, c.length] == c
-                    remaining = remaining.slice(c.length, remaining.length)
-                    visited[y + dy][x + dx] = true
-
-                    x += dx
-                    y += dy
-
-                    # we've found a match
-                    if remaining.empty?
-                      return true
-                    end
-
-                    found_next = true
-                  end
-                end
-              end
-            end
-          end
-        end
-      end
-    end
-
-    false
+    all_possible_search_trees.any? { |tree| tree.has_word?(word) }
   end
 
   private
 
-  def create_empty_visited
-    board.cells.map do |row|
-      row.map { false }
+  class SearchTree
+    attr_reader :board, :visited, :x, :y
+
+    def initialize(board:, visited:, x:, y:)
+      @board = board
+      @visited = visited
+      @x = x
+      @y = y
+    end
+
+    def has_word?(word)
+      word.start_with?( cell_string ) && all_valid_search_trees.any? do |tree|
+          remaining = word.slice(cell_string.length, word.length)
+          remaining.empty? || tree.has_word?(remaining)
+        end
+    end
+
+    private
+
+    def all_valid_search_trees
+      visited_copy = visited.clone
+      visited_copy[y][x] = true
+
+      (-1..1).map do |dy|
+        (-1..1).map do |dx|
+          if valid?(x + dx, y + dy) && !visited_copy[y + dy][x + dx]
+            SearchTree.new(board: board, visited: visited_copy, x: x + dx, y: y + dy)
+          end
+        end
+      end.flatten.compact
+    end
+
+    def valid?(x, y)
+      x >= 0 && y >= 0 && x < board.width && y < board.height
+    end
+
+    def cell_string
+      board.cell_at(x, y)
     end
   end
 
-  def valid?(x, y)
-    x >= 0 && y >= 0 && x < board.width && y < board.height
+  def all_possible_search_trees
+    (0...board.height).map do |start_y|
+      (0...board.width).map do |start_x|
+        SearchTree.new(board: board, visited: empty_visited, x: start_x, y: start_y)
+      end
+    end.flatten
+  end
+
+  def empty_visited
+    board.cells.map do |row|
+      row.map { false }
+    end
   end
 
 end
