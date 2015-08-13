0508548403b55016bbc41be6424aa3a3f32ea2d7
Jevon Wright <jevon@jevon.org>
2015-08-13 16:38:15 +1200

    Add basic specs for presenters

---
+++ spec/presenters/board_presenter_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe BoardPresenter, type: :service do
+  let(:user) { User.create! }
+  let(:game) { Game.create! }
+  let(:board) { game.boards.create! round_number: 0, serialized: "empty" }
+  let(:presenter) { BoardPresenter.new(board) }
+
+  before :each do
+    game.players.create! user: user
+    game.players.create! user: user
+  end
+
+  context "#to_json" do
+    let(:json) { presenter.to_json }
+
+    it "returns json" do
+      expect(json).to_not be_empty
+    end
+  end
+
+end
---
+++ spec/presenters/game_presenter_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+RSpec.describe GamePresenter, type: :service do
+  let(:user) { User.create! }
+  let(:game) { Game.create! }
+  let(:presenter) { GamePresenter.new(game) }
+
+  before :each do
+    game.players.create! user: user
+    game.players.create! user: user
+  end
+
+  context "#to_json" do
+    let(:json) { presenter.to_json }
+
+    it "returns json" do
+      expect(json).to_not be_empty
+    end
+
+    it "returns 2 players" do
+      expect(json[:players]).to eq(2)
+    end
+
+    it "returns 0 players ready" do
+      expect(json[:players_ready]).to eq(0)
+    end
+  end
+
+  context "#to_data_set" do
+    let(:dataset) { presenter.to_data_set }
+
+    it "returns data set" do
+      expect(dataset).to_not be_empty
+    end
+
+    it "has a data-check-url parameter" do
+      expect(dataset["data-check-url"]).to_not be_empty
+    end
+  end
+
+end
